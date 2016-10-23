269e2b71fd3fafa85684fb3c3583a0fc1b13304b
JoelBot <Joel.Lonneker@gmail.com>
2016-10-23 13:55:43 -0400

    long coming commit with structure, coloring, and previous times tally

--- js/global.js
+++ js/global.js
@@ -1,3 +1,4 @@
+
 // global variables
 var timer
 var timeCounter = 0
@@ -5,23 +6,35 @@ var readout = document.getElementById('readout')
 var paused = true
 var colorAugmentator = 0
 var resetTimeoutReset
+var timerMin = 0
+var timerSec = 0
+var timerTenth = 0
+var placeholderSec = ''
+var placeholderMin= ''
 
-// var timerMin = 0
-// var timerSec = 0
-// var timerTenth = 0
+
 
 // function mechanics
 function startTimer() {
-    timer = setInterval(incrementTimer, 10)
+    timer = setInterval(incrementTimer, 100)
 }
 
 function incrementTimer() {
-    readout.setAttribute('value',timeCounter)
+    drawTime()
     timeCounter++
-    // time_o_maticConversion_o_tron(timeCounter)
+    time_o_maticConversion_o_tron(timeCounter)
     textColorAugmentations()
 }
 
+function drawTime() {
+    readout.setAttribute('value', placeholderMin + timerMin + ':' + placeholderSec + timerSec + '.' + timerTenth)
+}
+function drawTimeCard(input) {
+    var timeCard = document.createElement('p')
+    timeCard.innerHTML = '&nbsp;' + placeholderMin + timerMin + ':' + placeholderSec + timerSec + '.' + timerTenth
+    // console.dir(timeCard)
+    input.appendChild(timeCard)
+}
 function textColorAugmentations() {
     if (colorAugmentator >= 255) {
         colorAugmentator = 0
@@ -36,6 +49,7 @@ function textColorAugmentations() {
 function start() { // starts the timer and clears timeout reset
     startTimer()
     document.getElementById('start').innerHTML = 'Pause'
+    readout.classList.remove('flash', 'animated', 'infinite')
     paused = false
     clearTimeout(resetTimeoutReset)
 }
@@ -44,33 +58,53 @@ function stop() { // stops the timer and begins 15 second timeout reset
     clearInterval(timer)
     document.getElementById('start').innerHTML = 'Continue'
     resetTimeoutReset = setTimeout(function() {
+        drawTimeCard(document.getElementById('previousTimes'))
         resetTimer()
         document.getElementById('start').innerHTML = 'Start'
     }, 15000)
+    readout.classList.add('flash', 'animated', 'infinite')
     paused = true
 }
 
 function resetTimer() {
     timeCounter = 0
+    timerMin = 0
+    timerSec = 0
+    timerTenth = 0
     incrementTimer()
 }
-// function time_o_maticConversion_o_tron(timeCount) {
-//     while (timeCount >= 0) {
-//         if (timeCount%6000) {
-//             timerMin += 1
-//         }
-//         if (timeCount%100) {
-//             timerSec += 1
-//         }
-//         if (timeCount%10) {
-//             timerTenth += 1
-//         }
-//
-//     }
-//     console.log(timerTenth)
-//     console.log(timerSec)
-//     console.log(timerMin)
-// }
+function time_o_maticConversion_o_tron() {
+    if (timeCounter%600 === 0) {
+        timerMin++
+        if (timerMin < 10) {
+            placeholderMin = "0"
+        }
+    }
+    if (timeCounter%10 === 0) {
+        timerSec++
+        if (timerSec === 60) {
+            timerSec = 0
+        }
+        if (timerSec < 1) {
+            placeholderSec = "0"
+        }
+        else {
+            placeholderSec = ''
+        }
+    }
+    if (timeCounter%1 === 0) {
+        timerTenth++
+        if (timerTenth === 10) {
+            timerTenth = 0
+        }
+        // if (timerTenth < 10) {
+        //     placeholderTenth = '0'
+        // }
+        else {
+            placeholderTenth = ''
+        }
+    }
+}
 // Event Listener Functions
 // Start and pause button
 document.getElementById('start').addEventListener('click', function() {
@@ -84,12 +118,14 @@ document.getElementById('start').addEventListener('click', function() {
 })
 
 document.getElementById('start').addEventListener('dblclick', function() {
+    drawTimeCard(document.getElementById('previousTimes'))
     resetTimer()
 })
 // Reset button only functions when timer is paused and it restarts the timeCounter to 0
 document.getElementById('reset').addEventListener('click', function() {
 
     if (paused) {//  checks to see if paused flag is true and resets the counter.
+        drawTimeCard(document.getElementById('previousTimes'))
         resetTimer()
     }
     else { // stops the intercal timer and pauses
